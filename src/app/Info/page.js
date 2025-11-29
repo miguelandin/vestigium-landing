5c25914c7602d16829c64bc31a1164a483cde9da
miguelandin <miguel.cabrera.landin@gmail.com>
2025-11-29 18:53:40 +0100

    Revert "FEAT: Title page"
    This reverts commit d94e465f85153323f07abef8388b8a84cb3bde8b.

--- src/app/Info/page.js
+++
@@ -1,3 +0,0 @@
-export default function Info() {
-    return (<a>Aqui va landing</a>)
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,7 @@
-"use client";
+import Image from "next/image";
 
-import { useState } from 'react';
-
-export default function Title() { // redirect button to the info presentation
-    const [isAnimating, setIsAnimating] = useState(false);
-    const [endAnimating, setEndAnimating] = useState(false);
-
-    const handleClick = () => {
-        setIsAnimating(true);
-        setTimeout(() => {
-            setEndAnimating(true)
-            window.location.href = "/Info";
-        }, 900);
-    };
-
-    return (
-        <div className="flex sm:items-center justify-center min-h-screen">
-            <img
-                src="/logo_web.webp"
-                className="invert cursor-pointer transition hover:scale-110"
-                alt="Ve10tigium"
-                onClick={handleClick}
-                style={{
-                    animation: isAnimating ? 'ping 1s ease-in-out' : 'none',
-                    opacity: endAnimating ? '0' : 'initial'
-                }}
-            />
-        </div>
-    );
+export default function Home() {
+  return (
+	  <a>Hello World</a>
+  );
 }
